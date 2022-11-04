eac613574a32549c282a204c04217160c608aa42
alfaonyt <62027587+alfaonyt@users.noreply.github.com>
2022-11-04 10:35:49 +0100

    Create lineage_anakin.mk

---
+++ lineage_anakin.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from the device configuration.
+$(call inherit-product, device/asus/anakin/device.mk)
+
+# Inherit from the Lineage configuration.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_BRAND := asus
+PRODUCT_DEVICE := anakin
+PRODUCT_MANUFACTURER := asus
+PRODUCT_MODEL := ASUS_I005D
+PRODUCT_NAME := lineage_anakin
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=ASUS_I006D \
+    TARGET_PRODUCT=WW_I005D
+
+BUILD_FINGERPRINT := asus/WW_I006D/ASUS_I006D:12/SKQ1.210821.001/31.1010.0411.113:user/release-keys
