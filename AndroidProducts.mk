5dcf62769b55164be833bd0eccd664b3c20679ed
alfaonyt <62027587+alfaonyt@users.noreply.github.com>
2022-11-04 10:28:10 +0100

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_anakin.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_anakin-eng \
+    lineage_anakin-user \
+    lineage_anakin-userdebug
