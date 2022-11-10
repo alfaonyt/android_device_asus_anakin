85a122dfe3341dfb232beb58470f54408ffcc092
alfaonyt <62027587+alfaonyt@users.noreply.github.com>
2022-11-10 07:41:02 +0100

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -72,5 +72,6 @@ function blob_fixup() {
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+extract "${MY_DIR}/proprietary-files-product.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh
